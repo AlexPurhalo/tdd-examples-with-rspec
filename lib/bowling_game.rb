55da750ad390f8cf4d14e873362459181a39b258
AlexPurhalo <alexpurhalo@gmail.com>
2017-02-04 00:15:57 +0200

    last-changes

--- lib/bowling_game.rb
+++ lib/bowling_game.rb
@@ -1,79 +1,37 @@
 class BowlingGame
-  # sets instance variable with current frame by default to zero
-  def initialize
-    @fir_val_in_frame = 0
-  end
+  def initialize; @fir_val_in_frame = 0; end
 
-  # fir_val_in_frame method - returns the value of @fir_val_in_frame instance
-  def fir_val_in_frame
-    @fir_val_in_frame
-  end
+  def fir_val_in_frame; @fir_val_in_frame; end
 
-  # pins method - counts the score
-  def pins(rolls) # takes the array with numbers as parameter
-    @rolls = rolls
-  end
+  def pins(rolls); @rolls = rolls; end
 
-  # score method - simply returns the value passed to instance variable
   def score
-    frame = 0 # default count of the frames
-    score = 0 # default value for score
+    i, score, fir_val = 0, 0, 0
 
-    while frame < 10 # works until the frames count won't achieve to ten\
-      if spare?
-        score += 10 +  spare_bonus # with spare bonus
-        @fir_val_in_frame += 2
-      elsif strike?
-        score += 10 + strike_bonus # with strike bonus
-        @fir_val_in_frame += 1
+    while i < 10
+      if @rolls[fir_val].to_i == 10
+        score += 10 + @rolls[fir_val + 1].to_i + @rolls[fir_val + 2].to_i
+        fir_val += 1
+      elsif @rolls[fir_val].to_i + @rolls[fir_val+1].to_i == 10
+        score += 10 + @rolls[fir_val + 2].to_i
+        fir_val += 2
       else
-        score += standard_frame_score  # simple situation
-        @fir_val_in_frame += 2
+        score += @rolls[fir_val].to_i + @rolls[fir_val + 1].to_i
+        fir_val += 2
       end
 
-      frame += 1
+      i += 1
     end
 
     score
   end
-
-  private
-  # spare method - returns true if spare, otherwise returns false
-  def spare?
-    # if sum of current and next frames is equal to ten, returns true, otherwise returns false
-    if @rolls[fir_val_in_frame].to_i + @rolls[fir_val_in_frame + 1].to_i == 10
-      true
-    else
-      false
-    end
-  end
-
-
-  def strike?
-    # if first fall is equal to ten
-    if @rolls[@fir_val_in_frame].to_i == 10
-      true
-    else
-      false
-    end
-  end
-
-  # standard_frame_score method -  returns sum of the values in the frame
-  def standard_frame_score
-    @rolls[fir_val_in_frame].to_i + @rolls[fir_val_in_frame + 1].to_i
-  end
-
-  # spare_bonus method - returns values that equal to first value in the next frame
-  def spare_bonus
-    @rolls[fir_val_in_frame + 2].to_i
-  end
-
-  def strike_bonus
-    @rolls[fir_val_in_frame + 1].to_i + @rolls[fir_val_in_frame + 2].to_i
-  end
 end
 
+game = BowlingGame.new
 
-# game = BowlingGame.new
-# game.pins([2, 5]) # takes array with numbers as argument
-# puts game.score
+# ([1,4] + [4,5] + [6,4,5] + [5,5,10] + [10,0,1] + [0,1])
+# ( 5    + 9(14) + 15(29)  + 20(49)   +  11(60)  + 1(61)
+game.pins([2,8,4,2,8,10,0,1])
+# game.pins([1,4] + [4,5] + [6,5] + [5,5] + [10] + [0,1])
+puts game.score
+# alex_attempts =  # 133
--- spec/bowling_game_spec.rb
+++ spec/bowling_game_spec.rb
@@ -18,9 +18,24 @@ describe BowlingGame do
     expect(game.score).to eq(20)
   end
 
+  it 'scores of simple try and spare try' do
+    game.pins([4,3] + [5,5] + [7,1]) # 4+3(7) + 5+5(10 +spare(7)) + 7+1(8) = 7 + 17 + 8 = 32
+    expect(game.score).to eq(32)
+  end
+
+  it 'scores of simple try and strike' do
+    game.pins([4,3] + [10] + [8,1]) # 4+3(7) + 10(strike + 8 + 1) + 8+1 = 7 + 19 + 9 = 35
+    expect(game.score).to eq(35)
+  end
+
+
   it 'scores a game with strike' do
     game.pins([10,5,3] + [0])
     expect(game.score).to eq(26)
+  end
+
+  it 'simple try + try with strike' do
+    game.pins([10,])
   end
 
   it 'scores a perfect game' do
