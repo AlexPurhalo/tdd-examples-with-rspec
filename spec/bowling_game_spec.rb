9441d6e5de9843849891648971ed017afda0ee00
AlexPurhalo <alexpurhalo@gmail.com>
2017-02-03 23:03:40 +0200

    perfect-game-spec

--- spec/bowling_game_spec.rb
+++ spec/bowling_game_spec.rb
@@ -1,27 +1,30 @@
 require_relative '../lib/bowling_game'
 
 describe BowlingGame do
+  let(:game) { BowlingGame.new }
+
   it 'scores a gutter game' do
-    game = BowlingGame.new
     game.pins([0])
     expect(game.score).to eq(0)
   end
 
   it 'scores a first move with two rolls' do
-    game = BowlingGame.new
     game.pins([2, 5])
     expect(game.score).to eq(7)
   end
 
   it 'scores a game with spare' do
-    game = BowlingGame.new
     game.pins([4,6,5] + [0])
     expect(game.score).to eq(20)
   end
 
   it 'scores a game with strike' do
-    game = BowlingGame.new
     game.pins([10,5,3] + [0])
     expect(game.score).to eq(26)
+  end
+
+  it 'scores a perfect game' do
+    game.pins([10]*12)
+    expect(game.score).to eq(300)
   end
 end
